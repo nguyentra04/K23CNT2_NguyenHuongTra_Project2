086eb4068603014980016ce16a23c1dce3ef2ab4
nguyentra04 <nguyentra8704@gmail.com>
2025-09-19 19:44:42 +0700

    Xóa thư mục LibraryManagement2

--- LibraryManagement2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace LibraryManagement2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-        public DateTime Timestamp { get; set; } 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
